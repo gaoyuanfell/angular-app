2fd79d26f448fa9c654cc089d45ae8ea093cab88
moka <gaoyuanfell@sina.com>
2017-07-05 23:22:48 +0800

    gh

--- git.sh
+++ git.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 echo "Start to publish..."
-rm -rf dist
-git subtree add --prefix dist yuan gh-pages
 npm run build:github
 git add .
 if [ $1 ]
@@ -9,7 +7,6 @@ if [ $1 ]
     git commit -am $1
 fi
 git commit -am 'gh'
-git subtree pull --prefix dist yuan coding-pages
 git subtree push --prefix dist yuan gh-pages
 git push yuan master
 echo "Success";
