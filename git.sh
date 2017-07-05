971e40a745cb45da2f29ab40d28e5b765699744b
moka <gaoyuanfell@sina.com>
2017-07-05 23:12:37 +0800

    gh

--- git.sh
+++ git.sh
@@ -7,6 +7,8 @@ if [ $1 ]
     git commit -am $1
 fi
 git commit -am 'gh'
+git subtree add --prefix dist yuan gh-pages
+git subtree pull --prefix dist yuan coding-pages
 git subtree push --prefix dist yuan gh-pages
 git push yuan master
 echo "Success";
